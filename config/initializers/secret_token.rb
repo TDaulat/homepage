c57ca35affe84e6f0061b667fb604c4f2fa1cfd3
Tim Daulat <tim_d5@hotmail.com>
2012-07-15 16:29:34 -0500

    Created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KungFuTimsPlace::Application.config.secret_token = '98dddfb72af445e59f1559896da8dc5c9a25c405877b840362d605221513ebc555a1567484669f581f852719b2a4ca1e3426cb151da40616764f2552df0d375d'
