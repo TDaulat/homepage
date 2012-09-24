e28662c20a2917054335204be888b50d8ba908ca
Tim Daulat <tim_d5@hotmail.com>
2012-09-24 14:38:37 -0500

    ADDED: login required filters to admin controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,20 @@ private
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def login_required
+    !!current_user || access_denied
+  end
+
+  def admin_login_required
+    current_user.try(:admin?) || access_denied
+  end
+
+  def access_denied
+    respond_to do |format|
+      format.html do
+        redirect_to root_path
+      end
+    end
+  end
 end
